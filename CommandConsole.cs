bb3a40ed4473128ceac77af42e0d9dfbe014f4f2
DavidZhang309 <david_zhang_309@hotmail.com>
2015-09-10 15:06:09 -0400

    Initial Commit - Command Console and etc
    -Added Command Console which receives commands and sends them to the
    registered command handler
    
    -Added command type CommandValue, which stores a string modifiable by
    command calls
    -Added command type: EventCommand, which raises an event when the
    command is called for function calls

---
+++ CommandConsole.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CoreFramework
+{
+    public class CommandConsole
+    {
+        private string history = "";
+
+        private Dictionary<string, ICommandHandler> commands;
+
+        public CommandConsole()
+        {
+            commands = new Dictionary<string, ICommandHandler>();
+            Prefix = " >";
+            StdoutPrefix = "[Console]";
+        }
+        public bool PrintToStdout { get; set; }
+        public string Prefix { get; set; }
+        public string StdoutPrefix { get; set; }
+
+        public void RegisterCommand(string commandName, ICommandHandler handle)
+        {
+            commands.Add(commandName, handle);
+        }
+        public void Print(string msg)
+        {
+            history += Prefix + msg + "\n";
+            if (PrintToStdout) System.Console.WriteLine(StdoutPrefix + Prefix + msg);
+        }
+        public void Call(string command, bool printCommand)
+        {
+            string trimmed = command.Trim();
+            int cmdNameIndex = trimmed.IndexOf(' '); 
+            string cmdName = "";
+            //get command name
+            if (cmdNameIndex == -1)
+                cmdName = trimmed;
+            else
+                cmdName = trimmed.Substring(0, trimmed.IndexOf(' '));
+
+            if (printCommand) Print(trimmed);
+
+            //check and execute
+            if (commands.ContainsKey(cmdName))
+            {
+                string[] args = cmdNameIndex == -1 ? new string[] {} : trimmed.Substring(cmdNameIndex + 1).Split();
+                try
+                {
+                    commands[cmdName].SetCommand(this, args);
+                }
+                catch (Exception ex)
+                {
+                    Print("Command handler for '" + cmdName + "' threw an unexpected error\n\t" + ex.Message);
+                }
+            }
+            else if (printCommand)
+            {
+                Print("Command '" + cmdName + "' does not exist");
+            }
+        }
+    }
+}
---
+++ CommandTypes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CoreFramework
+{
+    public interface ICommandHandler
+    {
+        void SetCommand(CommandConsole console, string[] args);
+    }
+
+    public class CommandValue : ICommandHandler
+    {
+        public void SetCommand(CommandConsole console, string[] args)
+        {
+            if (args.Length == 0)
+                console.Print("=" + Value);
+            else
+                Value = args[0];
+        }
+
+        public string Value { get; set; }
+    }
+
+    public class EventCmdArgs : EventArgs
+    {
+        public EventCmdArgs(string[] args)
+        {
+            Arguments = args;
+        }
+
+        public CommandConsole ConsoleCaller { get; private set; }
+        public string[] Arguments { get; private set; }
+    }
+    public class EventCommand : ICommandHandler
+    {
+        public event EventHandler<EventCmdArgs> OnCommand;
+
+        public EventCommand()
+        { }
+
+        public EventCommand(Action<object, EventCmdArgs> func)
+        {
+            OnCommand += new EventHandler<EventCmdArgs>(func);
+        }
+
+        public void SetCommand(CommandConsole console, string[] args)
+        {
+            if (OnCommand != null) OnCommand(this, new EventCmdArgs(args));
+        }
+    }
+}
